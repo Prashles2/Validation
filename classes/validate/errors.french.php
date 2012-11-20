f942633925d32598391b1395ea8816ee7f2a008c
Prash <prash.somaiya@gmail.com>
2012-11-20 16:52:34 +0000

    French support
    And readme

---
+++ classes/validate/errors.french.php
@@ -0,0 +1,17 @@
+<?php
+
+$errorText = array(
+	'min_length'     => 'Le :name doit être au moins :param caractères',
+	'email'          => 'Le :name doit être une adresse email valable',
+	'max_length'     => 'Le :name doit être moins de :param caractères',
+	'Requis'         => ':name est requis',
+	'ip'             => 'Le :name doit être une adresse IP valable',
+	'correspond'     => 'Le :name doit correspondre :param',
+	'match_exact'    => ':name doit correspondre exactement :param',
+	'match_password' => 'Les mots de passe ne correspondent pas',
+	'alphanum'       => 'Le :name doit contenir seulement des nombres et des lettres alpha-numeriques',
+	'url'            => 'Le :name doit être une URL valable',
+	'numerique'      => 'Le :name doit être numerique',
+	'min'            => ':name doit être au moins :param',
+	'max'            => ':name doit être moins que :param'
+);
--- validate.php
+++ validate.php
@@ -6,9 +6,11 @@ $val = new Validate;
 
 $invalidEmail  = 'foo';
 $shortPassword = 'sdfsd';
+$validEmail    = 'foo@bar.com';
 
 $val->rule($invalidEmail, 'Email', 'required|email'); // Will return false
 $val->rule($shortPassword, 'Password', 'min_length[10]|max_length[25]');
+$val->rule($validEmail, 'Email2', 'required|email'); // Wil return true
 
 $errors = $val->errors();
 if (!$errors) {
@@ -19,41 +21,3 @@ else {
 		echo "{$error}<br/>";
 	}
 }
-
-/*
-$val = new Validate;
-
-$email     = 'invalid@email';
-$url       = 'invalidUrlHere';
-$ip        = '123123123';
-$firstName = ''; 
-$lastName  = 'a';
-
-$val->rule($email, 'Email', 'email');
-$val->rule($url, 'URL', 'required|url');
-$val->rule($ip, 'IP Address', 'required|ip');
-$val->rule($firstName, 'First name', 'required');
-$val->rule($lastName, 'Last name', 'min_length[4]');
-
-$exec = $val->errors();
-
-if (!$exec) {
-	echo 'Success!';
-}
-else {
-	foreach ($exec as $error) {
-		echo "{$error}<br/>";
-	}
-}
-
-/* Output:
-Email should be a valid e-mail address
-URL should be a valid URL
-IP Address should be a valid IP address
-First name is required
-*/
-
-/* 
-* Rules set, functions respond true or false for each
-* Add to errors class
-*/
