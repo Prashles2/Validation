058a9f538470dbc5f438d95935ea0e0501c2fa6e
Prash <prash.somaiya@gmail.com>
2012-11-20 17:18:37 +0000

    Added German support

---
+++ classes/validate/errors.german.php
@@ -0,0 +1,17 @@
+<?php
+
+$errorText = array(
+	'min_length'     => ':name muss mindestens :param Buchstaben lang sein',
+	'email'          => ':name muss eine gültige E-mail adresse sein',
+	'max_length'     => ':name muss kleiner als :param Buchstaben lang sein',
+	'Erforderlich'   => ':name es erforderlichist',
+	'ip'             => ':name muss eine erforderliche IP Adresse sein',
+	'match'          => ':name es :param zusammenpassen muss',
+	'match_exact'    => ':name muss :param präziselich zusammenpassen',
+	'match_password' => 'Kennwörter zusammenpassen nicht',
+	'alphanum'       => ':name er muss alphanumerisch sein',
+	'url'            => ':name er muss eine erforderliche URL-Adresse sein',
+	'numeric'        => ':name er muss numerisch sein',
+	'min'            => ':name er muss mindestens :param sein',
+	'max'            => ':name es muss nicht mehr als :param sein'
+);
