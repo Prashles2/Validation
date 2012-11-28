30671eef77d44430aaa170c8ec50a4a4c8ff6147
Prash <prash.somaiya@gmail.com>
2012-11-28 13:54:42 +0000

    Dutch

---
+++ classes/validate/errors.dutch.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+* Credits to @Jilles for Dutch translation
+*/
+
+$errorText = array(
+    'min_length'     => ':name moet minimaal :param karakters lang zijn',
+    'email'          => ':name moet een geldig e-mail adres zijn',
+    'max_length'     => ':name mag niet langer zijn dan :param karakters',
+    'required'       => ':name is verplicht',
+    'ip'             => ':name moet een geldig IP adres zijn',
+    'match'          => ':name moet overeenkomen met :param',
+    'match_exact'    => ':name moet exact overeenkomen met :param',
+    'match_password' => 'Wachtwoorden komen niet overeen',
+    'alphanum'       => ':name moet een alpha-numerieke text zijn',
+    'url'            => ':name moet een geldige URL zijn',
+    'numeric'        => ':name moet numeriek zijn',
+    'min'            => ':name moet minimaal :param zijn',
+    'max'            => ':name mag maximaal :param zijn'
+); 
